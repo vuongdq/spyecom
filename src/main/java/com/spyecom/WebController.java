b28b475b77043404d76b2fe1f0f3fcdf918f902c
vuongdq <quocvuong_it@yahoo.com>
2022-08-20 21:10:49 +0700

    New Project

--- src/main/java/com/spyecom/WebController.java
+++ src/main/java/com/spyecom/WebController.java
@@ -1,4 +1,15 @@
 package com.spyecom;
 
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("")
 public class WebController {
+    @GetMapping("/home")
+    public String homePage(){
+        return "home";
+    }
 }
